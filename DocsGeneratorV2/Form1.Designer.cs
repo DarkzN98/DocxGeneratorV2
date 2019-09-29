76202ab40e68113ce6c14e8562f1f80d02db290c
unknown <darkzn98@gmail.com>
2019-09-29 18:48:50 +0700

    Finished Create Docx Generator

---
+++ DocsGeneratorV2/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace DocsGeneratorV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtnrp = new System.Windows.Forms.TextBox();
+            this.txtprak = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(226, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select Files";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(226, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Generate";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "docx";
+            this.saveFileDialog1.Filter = "Word Document|*.docx";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.groupBox1.Controls.Add(this.txtprak);
+            this.groupBox1.Controls.Add(this.txtnrp);
+            this.groupBox1.Controls.Add(this.txtname);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox1.Location = new System.Drawing.Point(12, 14);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 138);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(24, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NRP:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(23, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Prak:";
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(71, 35);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(112, 20);
+            this.txtname.TabIndex = 3;
+            // 
+            // txtnrp
+            // 
+            this.txtnrp.Location = new System.Drawing.Point(71, 61);
+            this.txtnrp.Name = "txtnrp";
+            this.txtnrp.Size = new System.Drawing.Size(112, 20);
+            this.txtnrp.TabIndex = 4;
+            // 
+            // txtprak
+            // 
+            this.txtprak.Location = new System.Drawing.Point(71, 87);
+            this.txtprak.Name = "txtprak";
+            this.txtprak.Size = new System.Drawing.Size(112, 20);
+            this.txtprak.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(385, 164);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "DOCX GEN V2";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtprak;
+        private System.Windows.Forms.TextBox txtnrp;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DocsGeneratorV2/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
+using System.IO;
+
+using System.Windows.Forms;
+
+namespace DocsGeneratorV2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String nama, nrp, prakname;
+
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                String[] filenames = openFileDialog1.FileNames;
+                String savePath = Path.GetDirectoryName(saveFileDialog1.FileName) + "/";
+
+                var app = new Microsoft.Office.Interop.Word.Application();
+                app.Visible = false;
+                
+                var doc = app.Documents.Add(Type.Missing);
+
+                //set paper size
+                doc.PageSetup.PaperSize = WdPaperSize.wdPaperA4;
+                doc.PageSetup.BottomMargin = app.CentimetersToPoints((float)1);
+                doc.PageSetup.LeftMargin = app.CentimetersToPoints((float)1);
+                doc.PageSetup.TopMargin = app.CentimetersToPoints((float)1);
+                doc.PageSetup.RightMargin = app.CentimetersToPoints((float)1);
+
+                //set spacing after and before
+                doc.Paragraphs.SpaceAfter = 0;
+                doc.Paragraphs.SpaceBefore = 0;
+
+                foreach (String s in openFileDialog1.FileNames)
+                {
+                    // Write Title
+                    var para = doc.Paragraphs.Last;
+                    var range = para.Range;
+                    range.Font.Bold = 1;
+                    range.Font.Underline = WdUnderline.wdUnderlineSingle;
+                    range.Font.Size = 14;
+                    range.Font.Name = "Courier New";
+                    range.InsertAfter(Path.GetFileName(s));
+
+                    //Write Enter
+                    doc.Paragraphs.Add();
+                    para = doc.Paragraphs.Last;
+                    range = para.Range;
+                    range.Font.Bold = 1;
+                    range.Font.Size = 10;
+
+                    //write content
+                    doc.Paragraphs.Add();
+                    para = doc.Paragraphs.Last;
+                    range = para.Range;
+                    range.Font.Bold = 0;
+                    range.Font.Size = 10;
+                    range.Font.Name = "Courier New";
+                    range.InsertAfter(File.ReadAllText(s));
+
+                    doc.Paragraphs.Add();
+                }
+
+                // set Footer
+                doc.ActiveWindow.ActivePane.View.SeekView = WdSeekView.wdSeekCurrentPageFooter;
+                doc.ActiveWindow.ActivePane.Selection.Paragraphs.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+                Object currentPage = WdFieldType.wdFieldPage;
+                doc.ActiveWindow.Selection.Fields.Add(doc.ActiveWindow.Selection.Range, ref currentPage);
+
+                // set Header
+                doc.ActiveWindow.ActivePane.View.SeekView = WdSeekView.wdSeekCurrentPageHeader;
+                doc.ActiveWindow.ActivePane.Selection.Paragraphs.Alignment = WdParagraphAlignment.wdAlignParagraphLeft;
+                Object currentPage2 = WdFieldType.wdFieldPage;
+                doc.ActiveWindow.Selection.TypeText($"[{txtname.Text} - {txtnrp.Text} - {txtprak.Text}]");
+
+                doc.SaveAs2(saveFileDialog1.FileName);
+                doc.Close();
+                app.Quit();
+
+                MessageBox.Show("GENERATE SUCCESS!");
+            }
+        }
+    }
+}
